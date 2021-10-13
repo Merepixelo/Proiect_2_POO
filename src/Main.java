5fcc389c2b6420d3eed182d243d8f612b85f7e6c
Alex <meraalex2001@gmail,com>
2021-10-13 14:37:07 +0300

    Finished idea and concepts required by the guidelines

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/* Tales form the Wastelands ___ gatcha game
+
+Timp:
+Start: 11.10.2021
+End: 20.10.2021
+
+Verificare:
+Upload: Github
+Diagrame UML
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+Cerinte implementate in proiect:
+
+->get/set(): currency for rolling the gacha
+->metodelor:-private: Nume,base_atk,base_def,base_hp,
+            -public: upgrade_atk,upgrade_def,upgrade_hp
+            -protected: max_atk,max_def,max_hp
+->
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Cerințe:
+
+->Prog. de baza:
+Folosirea claselor, constructorilor, membrilor, metodelor(publice, private, protected), modificatori de access
+Instantare obiecte, Incapsulare
+Metode de clasa/instanta, get/set(), equals(), toString()
+Pachete, interfete, vectori/tablouri de obiecte/structuri de date
+
+Moștenire + polimorfism
+
+Supraîncarcarea/supradefinirea metodelor
+
+->Viusal:
+Aplicație cu interfață grafică folosind Java Swing
+Folosirea obiectelor din Java Swing, ActionListeners, OptionPane
+
+
+
+ */
---
+++ src/Units/Thorn.java
@@ -0,0 +1,24 @@
+package Units;
+
+public class Thorn {
+    private String nume="Thorn the Unbound";
+
+    private double chance=1.0;
+
+    private double base_atk=0.0;
+    private double base_def=0.0;
+    private double base_hp=0.0;
+
+    public double upgrade_atk=0.0;
+    public double upgrade_def=0.0;
+    public double upgrade_hp=0.0;
+
+    protected double max_atk=0.0;
+    protected double max_def=0.0;
+    protected double max_hp=0.0;
+
+
+
+
+
+}
